26960bab651eb100b31f982bdb1696d84bf770a3
paul.v.estipona.ctr <paul_estipona@yahoo.com>
2023-01-21 07:18:18 -0500

    Added Project to GitHub

--- app/src/main/java/com/pestipona/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/pestipona/kidsdrawingapp/DrawingView.kt
@@ -8,6 +8,7 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import android.graphics.Path
+import android.view.MotionEvent
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
@@ -19,10 +20,12 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var color = Color.BLACK
     private var canvas: Canvas? = null
 
+    /* This function calls setUpDrawing to initialize the private variables */
     init {
         setUpDrawing()
     }
 
+    /* This function sets the values for all private variables */
     private fun setUpDrawing() {
         mDrawPaint = Paint()
         mDrawPath = CustomPath(color, mBrushSize)
@@ -38,6 +41,60 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         super.onSizeChanged(w, h, oldw, oldh)
         mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
         canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    // Change Canvas to Canvas? if fails
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        if (!mDrawPath!!.isEmpty) {
+            // color and thickness of the paint
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        // when finger is pressed on screen
+        // 3 important events happen
+        when(event?.action){
+            // when finger is pressed down on the screen
+            MotionEvent.ACTION_DOWN -> {
+                // color and thickness of the path
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                // reset value for next event
+                mDrawPath!!.reset()
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.moveTo(touchX, touchY)
+                    }
+                }
+            }
+            // when finger is dragged on the screen
+            MotionEvent.ACTION_MOVE -> {
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.lineTo(touchX, touchY)
+                    }
+                }
+            }
+            // when finger is lifted off the screen
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            // catch all since 'when' event, since there are other possible events
+            else -> return false
+        }
+        invalidate()
+
+        return true
     }
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float): Path(){
