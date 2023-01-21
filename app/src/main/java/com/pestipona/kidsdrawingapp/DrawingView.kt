3790b7314020e2664441292b0d9c4470aa02abec
paul.v.estipona.ctr <paul_estipona@yahoo.com>
2023-01-21 00:59:57 -0500

    Added Project to GitHub

---
+++ app/src/main/java/com/pestipona/kidsdrawingapp/DrawingView.kt
@@ -0,0 +1,8 @@
+package com.pestipona.kidsdrawingapp
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+}
