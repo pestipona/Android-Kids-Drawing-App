2c8e6d42a38ecef737513e1b3c0277e0aa84d9d8
paul.v.estipona.ctr <paul_estipona@yahoo.com>
2023-01-21 06:43:31 -0500

    Added Project to GitHub

--- app/src/main/java/com/pestipona/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/pestipona/kidsdrawingapp/DrawingView.kt
@@ -34,6 +34,12 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mBrushSize = 20.toFloat()
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
     internal inner class CustomPath(var color: Int, var brushThickness: Float): Path(){
 
     }
