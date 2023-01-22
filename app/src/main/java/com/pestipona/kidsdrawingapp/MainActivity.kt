ff40267229af59243053f3fc65b72cacbaf62846
paul.v.estipona.ctr <paul_estipona@yahoo.com>
2023-01-21 20:36:26 -0500

    Added ReadMe to Project in GitHub

--- app/src/main/java/com/pestipona/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/pestipona/kidsdrawingapp/MainActivity.kt
@@ -4,16 +4,26 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val linearLayoutPaintColors: LinearLayout = findViewById(R.id.ll_paint_colors)
+        mImageButtonCurrentPaint = linearLayoutPaintColors[12] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
 
         val ib_brush: ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
